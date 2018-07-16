2b2ef12c22b4c1958ec9b87a6d2c7afb298bc2dc
slowdream <slowdream@yandex.ru>
2018-07-16 15:20:54 +0300

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+                        @csrf
 
                         <div class="form-group{{ $errors->has('login') ? ' has-error' : '' }}">
                             <label for="login" class="col-md-4 control-label">Логин</label>
