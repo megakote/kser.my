19535775848ab40e2148aa2862452d6f2cced287
slowdream <slowdream@yandex.ru>
2018-07-30 17:11:44 +0300

    commit

--- database/seeds/MenuSeeder.php
+++ database/seeds/MenuSeeder.php
@@ -15,63 +15,128 @@ class MenuSeeder extends Seeder
         Menu::create([
             'place' => 'header',
             'title' => 'Верхнее',
-            'url' => ''
+            'url'   => ''
         ]);
         Menu::create([
             'parent_id' => '1',
-            'title' => 'Главная',
-            'url' => '/'
+            'title'     => 'Главная',
+            'url'       => '/'
         ]);
         Menu::create([
             'parent_id' => '1',
-            'title' => 'Статьи',
-            'url' => '/articles'
+            'title'     => 'Статьи',
+            'url'       => '/articles'
         ]);
         Menu::create([
             'parent_id' => '1',
-            'title' => 'Отзывы',
-            'url' => '/feedback'
+            'title'     => 'Отзывы',
+            'url'       => '/feedback'
         ]);
         Menu::create([
             'parent_id' => 1,
-            'title' => 'Адреса сервисных центров',
-            'url' => '/contacts'
+            'title'     => 'Адреса сервисных центров',
+            'url'       => '/contacts'
         ]);
         Menu::create([
             'parent_id' => 1,
-            'title' => 'О нас',
-            'url' => '/about'
+            'title'     => 'О нас',
+            'url'       => '/about'
         ]);
         $footer1 = Menu::create([
             'place' => 'footer1',
             'title' => 'Нижнее1',
-            'url' => ''
+            'url'   => ''
         ]);
         Menu::create([
             'parent_id' => $footer1->id,
-            'title' => 'Отзывы',
-            'url' => '/feedback'
+            'title'     => 'Заправка картриджей',
+            'url'       => '/'
         ]);
         Menu::create([
             'parent_id' => $footer1->id,
-            'title' => 'Адреса сервисных центров',
-            'url' => '/contacts'
+            'title'     => 'Замена картриджа',
+            'url'       => '/'
         ]);
         Menu::create([
             'parent_id' => $footer1->id,
-            'title' => 'О нас',
-            'url' => '/about'
+            'title'     => 'Вечный картридж',
+            'url'       => '/'
         ]);
+        Menu::create([
+            'parent_id' => $footer1->id,
+            'title'     => 'Ремонт принтеров',
+            'url'       => '/'
+        ]);
+        Menu::create([
+            'parent_id' => $footer1->id,
+            'title'     => 'Обслуживание принтеров',
+            'url'       => '/'
+        ]);
+        Menu::create([
+            'parent_id' => $footer1->id,
+            'title'     => 'Ремонт плоттеров',
+            'url'       => '/'
+        ]);
+        Menu::create([
+            'parent_id' => $footer1->id,
+            'title'     => 'Покопийное обслуживание',
+            'url'       => '/'
+        ]);
+        Menu::create([
+            'parent_id' => $footer1->id,
+            'title'     => 'Другие услуги',
+            'url'       => '/'
+        ]);
+
+
         $footer2 = Menu::create([
             'place' => 'footer2',
             'title' => 'Нижнее2',
-            'url' => ''
+            'url'   => ''
         ]);
+
 
         Menu::create([
             'parent_id' => $footer2->id,
-            'title' => 'Новости',
-            'url' => '/news'
+            'title'     => 'Услуги',
+            'url'       => '/'
         ]);
+        Menu::create([
+            'parent_id' => $footer2->id,
+            'title'     => 'Главная',
+            'url'       => '/'
+        ]);
+        Menu::create([
+            'parent_id' => $footer2->id,
+            'title'     => 'О компании',
+            'url'       => '/about'
+        ]);
+        Menu::create([
+            'parent_id' => $footer2->id,
+            'title'     => 'Акции',
+            'url'       => '/'
+        ]);
+        Menu::create([
+            'parent_id' => $footer2->id,
+            'title'     => 'Новости',
+            'url'       => '/news'
+        ]);
+        Menu::create([
+            'parent_id' => $footer2->id,
+            'title'     => 'Отзывы',
+            'url'       => '/feedback'
+        ]);
+        Menu::create([
+            'parent_id' => $footer1->id,
+            'title'     => 'Статьи',
+            'url'       => '/'
+        ]);
+        Menu::create([
+            'parent_id' => $footer1->id,
+            'title'     => 'Контакты',
+            'url'       => '/contacts'
+        ]);
+
+
     }
 }
